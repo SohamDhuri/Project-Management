830a38bf5ed65f5cea1742b0fb68162a1d9005db
Soham Dhuri <sohamdhuri17@gmail.com>
2026-03-22 22:59:30 +0530

    second commit

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,17 +1,13 @@
-import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
 
-import ws from 'ws';
-neonConfig.webSocketConstructor = ws;
+const globalForPrisma = globalThis;
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL,
-})
-export const prisma = new PrismaClient({ adapter })
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
-
