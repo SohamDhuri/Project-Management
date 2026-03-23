ceca38ae92d13be315cd5bcf1608300db8ac3b78
Soham Dhuri <sohamdhuri17@gmail.com>
2026-03-24 00:18:44 +0530

    fix prisma adapter config

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,12 +1,22 @@
 import pkg from '@prisma/client';
-
 const { PrismaClient } = pkg;
+
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+
+// ✅ pass connection string
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const globalForPrisma = globalThis;
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
