c7132782cca628a6b4f91de58025d2e6b21989a9
Soham Dhuri <sohamdhuri17@gmail.com>
2026-03-23 23:22:59 +0530

    fix prisma v7 build issue

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -4,7 +4,7 @@ export default defineConfig(
     {
     schema: 'prisma/schema.prisma',
     datasource: {
-        url: "postgresql://neondb_owner:npg_Zm3whc4QtJSN@ep-wispy-thunder-a1bssgk4-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+        url: process.env.DATABASE_URL,
     },
     });
 
