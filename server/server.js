1c56205d18073cc273870d4e392b8b192ab93254
Soham Dhuri <sohamdhuri17@gmail.com>
2026-04-21 01:38:38 +0530

    server change

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
